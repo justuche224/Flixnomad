cd4ca9cbb720aefa73c84ee86461c0d4ffc4430b
Uchechukwu-jr <uchechukwu644@gmail.com>
2024-03-11 03:03:25 +0100

    using a dummy genre

--- app/page.jsx
+++ app/page.jsx
@@ -2,32 +2,33 @@ import MovieList from "@/components/movies/MovieList";
 import Hero from "@/components/Hero";
 import { MovieCarousel } from "@/components/movies/MovieCarousel";
 import { baseUrl } from "@/utils/baseUrl";
+import { moviesdb } from "@/database/db-main-movie";
 
-async function getMovies() {
-  try {
-    const response = await fetch(`${baseUrl}/api/movies`, {
-      method: "GET",
-    });
-    if (!response.ok) {
-      throw new Error(`HTTP error ${response.status}`);
-    }
-    return response.json();
-  } catch (error) {
-    console.error("Error fetching movies:", error);
-    return { error: "Failed to load movies" };
-  }
-}
+// async function getMovies() {
+//   try {
+//     const response = await fetch(`${baseUrl}/api/movies`, {
+//       method: "GET",
+//     });
+//     if (!response.ok) {
+//       throw new Error(`HTTP error ${response.status}`);
+//     }
+//     return response.json();
+//   } catch (error) {
+//     console.error("Error fetching movies:", error);
+//     return { error: "Failed to load movies" };
+//   }
+// }
 
 export default async function Home() {
-  const movie = await getMovies();
+  //const movie = await getMovies();
   return (
     <main>
       <Hero />
       <div className="my-5 pl-5">
         <h1 className="text-3xl">Trending Movies</h1>
       </div>
-      <MovieCarousel movies={movie} />
-      <MovieList movies={movie} />
+      <MovieCarousel movies={moviesdb} />
+      <MovieList movies={moviesdb} />
     </main>
   );
 }
--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "https://flixnomad.vercel.app";
+export const baseUrl = "http://localhost:3000";
