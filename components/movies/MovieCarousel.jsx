83972f9c844d9e871e212730dae285ee898aebe1
Uchechukwu-jr <uchechukwu644@gmail.com>
2024-03-14 11:36:05 +0100

    made trending wider

--- components/movies/MovieCarousel.jsx
+++ components/movies/MovieCarousel.jsx
@@ -5,7 +5,7 @@ export const MovieCarousel = ({ movies }) => {
   const trending = movies.slice(0, 12);
 
   return (
-    <div className="mx-auto p-2 gap-2 trending w-[80%]">
+    <div className="mx-auto p-2 gap-2 trending w-[97%]">
       {trending.map((movie) => (
         <div key={movie.name} className="rounded-md">
           <Link href={`movies/${movie.id}`}>
