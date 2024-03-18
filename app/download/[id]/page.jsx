3e765fbd3ac602bc80c813798882a3ac335d4c47
Uchechukwu-jr <uchechukwu644@gmail.com>
2024-03-18 10:01:22 +0100

    changed max to 20

--- app/download/[id]/page.jsx
+++ app/download/[id]/page.jsx
@@ -24,13 +24,25 @@ async function FindMovie(id) {
     }
     return (
       <MovieInfo>
-        <h1 className="text-center text-2xl font-bold">
-          {movie.name} <span className="ml-1 italic">{movie.quality}</span>
+        <h1 className="text-center text-3xl font-bold">
+          {movie.name}{" "}
+          {movie.type == "series" ? (
+            <span className="ml-1 italic">{movie.runtime}</span>
+          ) : null}{" "}
+          <span className="ml-1 italic">{movie.quality}</span>
         </h1>
+        {movie.type == "series" ? (
+          <h2 className="text-center text-sm font-bold italic text-red-200 my-2">
+            Please note that series are currently available for download through
+            a telegram bot. clcik the link and it will take you to a telegram
+            bot, press start to download. sorry for any inconveniences
+          </h2>
+        ) : null}
         <div className="grid place-content-center">
           <Link
             className="my-2 text-center bg-red-500 border border-transparent px-3 py-1 rounded-md hover:text-red-500 hover:bg-transparent hover:border hover:border-red-500 transition-all duration-300"
             href={movie.downloadLink}
+            target="_blank"
           >
             Download
           </Link>
